a8108e23a56e278adc3c0cf011633dba79c22182
Worawut Boontan <worawut@theiconweb.com>
2019-11-10 13:36:56 +0700

    Flow Jest Next

---
+++ pages/index.js
@@ -0,0 +1,12 @@
+// @flow
+import React from 'react'
+import Page from '../components/Page'
+import '../style.less';
+
+export default () => (
+  <div className="container container-table">
+    <Page>
+      <div>Hello World</div>
+    </Page>
+  </div>
+)
